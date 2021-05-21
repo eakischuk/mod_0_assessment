b989f91f80557beaa35a7e4afb3cc8977377b7b2
Erika Kischuk <erika.kischuk@gmail.com>
2021-05-21 11:19:18 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,11 @@
+animal = "bunny"
+is_cuddly = true
+animal = "elephant"
+
+num_8 = 0.5
+num_3 = 22
+quotient = num_3 / num_8
+
+favorite_films = ['Nausicaa of the Valley of the Wind', 'Life of Pi', 'Lord of the Rings trilogy']
+favorite_films.shift
+puts animal.length
