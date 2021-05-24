5bb0c50ad5af2394e55ed4b3c589306b521d547b
Erika Kischuk <erika.kischuk@gmail.com>
2021-05-24 09:38:36 -0600

    Add commas to laptop and house hash

--- models/model_a.rb
+++ models/model_a.rb
@@ -1,10 +1,10 @@
 erikak_turing_laptop = {
-  "laptop_color" => 'grey'
-  "laptop_brand" => 'apple'
-  "num_of_keys" => 65
-  "months_owned" => 1.5
-  "has_touchscreen" => false
-  "is_small" => true
-  "connected_devices" => ['aeropex open ear headphones', 'sennheiser noise cancelling headphones']
+  "laptop_color" => 'grey',
+  "laptop_brand" => 'apple',
+  "num_of_keys" => 65,
+  "months_owned" => 1.5,
+  "has_touchscreen" => false,
+  "is_small" => true,
+  "connected_devices" => ['aeropex open ear headphones', 'sennheiser noise cancelling headphones'],
   "stickers_added" => ['dbc', 'doomslug', 'koloss']
 }
--- models/model_b.rb
+++ models/model_b.rb
@@ -1,10 +1,10 @@
 dream_house = {
-  house_location: 'Rocky Mountains'
-  house_color: 'brown'
-  number_of_rooms: 4
-  number_of_bathrooms: 2.5
-  has_yard: true
-  has_garage: true
-  special_features: ['kilter board', 'ceramics shed', 'chicken coop']
+  house_location: 'Rocky Mountains',
+  house_color: 'brown',
+  number_of_rooms: 4,
+  number_of_bathrooms: 2.5,
+  has_yard: true,
+  has_garage: true,
+  special_features: ['kilter board', 'ceramics shed', 'chicken coop'],
   house_residents: ['Erika', 'Brent', '3 cats', 'chickens', '1 dog']
 }
